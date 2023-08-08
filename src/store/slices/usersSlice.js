7bc2d0fd6e6928916d2a012bc7efbb3ae45e7311
Leeyanhawrt <leeyan1108@gmail.com>
2023-08-08 12:30:59 -0400

    Fetch users using ASYNC thunk

--- src/store/slices/usersSlice.js
+++ src/store/slices/usersSlice.js
@@ -1,11 +1,27 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { fetchUsers } from "../thunks/fetchUsers";
 
 const usersSlice = createSlice({
   name: 'users',
   initialState: {
     usersList: [],
+    error: null,
+    isLoading: false,
   },
-  reducers: {}
+  reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.pending, (state, action) => {
+      state.isLoading = true;
+    })
+    builder.addCase(fetchUsers.fufilled, (state, action) => {
+      state.isLoading = false;
+      state.usersList = action.payload;
+    });
+    builder.addCase(fetchUsers.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.error;
+    });
+  }
 })
 
 export const usersReducer = usersSlice.reducer;
